e1178df69e9b9ba90b76f23e49a34cd510cb6298
gamefensi <38190915+gamefensi@users.noreply.github.com>
2021-09-25 17:05:40 -0700

    text box added

--- js/gallery.js
+++ js/gallery.js
@@ -7,8 +7,9 @@ var openList = "<li id='photo'>"; //Declare a variable to contain an open list t
 var closeList = "</li>"; //Declare a variable to contain a close list tag
 var openCaptionTag = "<figcaption>";
 var closeCaptionTag = "</figcaption>";
-var openDescTag = "<div class='desc'>";
-var closeDescTag = "</div>"
+var openDescTag = "<div id='descBox' class='desc' onclick='displayInfo(";
+var closeDescTag = ")'>"
+var closeDescTag2 = "</div>"
 
 // Pet 1 info
 var petHdr1 = "Stacy"
@@ -22,27 +23,27 @@ var petCap2 = "Josh <p>Sex: N<br>Age: 6 Years</p>";
 var petDesc2 = "Josh is a 6 year old Fawn White Boxer Mix.";
 var petInfo2 = "Josh's info";
 // Pet 3 info
-var petHdr3 = "Josh"
+var petHdr3 = "Jake"
 var petCap3 = "Jake  <p>Sex: N<br>Age: 11 Years</p>";
 var petDesc3 = "Jake is an 11 year old Black White Siberian Husky";
 var petInfo3 = "Jake's info";
 // Pet 4 info
-var petHdr4 = "Josh"
+var petHdr4 = "Kevin"
 var petCap4 = "Kevin  <p>Sex: N<br>Age: 6 Years</p>";
 var petDesc4 = "Kevin is a 6 year old White Siberian Husky";
 var petInfo4 = "Kevin's info";
 // Pet 5 info
-var petHdr5 = "Josh"
+var petHdr5 = "Trunks"
 var petCap5 = "Trunks <p>Sex: M<br>Age: 6 Years</p>";
 var petDesc5 = "Trunks is a 6 year old Black White Poodle Min Havanese";
 var petInfo5 = "Trunks's info";
 // Pet 6 info
-var petHdr6 = "Josh"
+var petHdr6 = "Chris"
 var petCap6 = "Chris <p>Sex: M<br>Age: 8 Years</p>";
 var petDesc6 = "Chris is a 8 year old Fawn Black German Shepherd Mix";
 var petInfo6 = "Chris's info"
 // Pet 7 info
-var petHdr7 = "Josh"
+var petHdr7 = "Jesse"
 var petCap7 = "Jesse  <p>Sex: M<br>Age: 4 Years</p>";
 var petDesc7 ="Jesse is a 4 year old Black Brown Germ Shepherd Siberian Husky";
 var petInfo7 = "Jesse's info";
@@ -61,8 +62,9 @@ var petHdr10 = "Christina"
 var petCap10 = "Christina<p>Sex: N<br>Age: 7 Years</p>";
 var petDesc10 = "Christina is a 7 year old Black Shetid Sheepdog Mix";
 var petInfo10 = "Christina's info";
+var openInfoTag = "<div class='infoBox'> ";
+var closeInfoTag = "</div>";
 
-var boxOpen = "<"
 var captionTexts = [
   petCap1,
   petCap2,
@@ -88,14 +90,7 @@ var descTexts = [
   petDesc10,
 ];
 
-var getInfoBox = document.getElementById("infoBox"); 
-var getBoxHeader = document.getElementById("boxHeader");
-var getBoxTxt = document.getElementById("boxTxt");
-var getBoxcloseBox = document.getElementById("closeBox");
-var openInfoTag = "<div id='InfoBox' onclick='displayInfo("
-var closeInfoTag = ")'></div>"
-
-var boxTxtList = [
+var petTxts= [
   petInfo1,
   petInfo2,
   petInfo3,
@@ -108,6 +103,19 @@ var boxTxtList = [
   petInfo10
 ]; 
 
+var petHdrs = [
+  petHdr1,
+  petHdr2,
+  petHdr3,
+  petHdr4,
+  petHdr5,
+  petHdr6,
+  petHdr7,
+  petHdr8,
+  petHdr9,
+  petHdr10
+];
+
 //Create a loop to create 6 images starting with index of 0
 for (var i = 0; i < numOfPhotos; i++) {
   fileNames.push("petphoto" + (i + 1)); //create image file name & store in array
@@ -116,11 +124,12 @@ for (var i = 0; i < numOfPhotos; i++) {
     openList +
     photos[i] +
     openDescTag +
-    descTexts[i] +
+    i +
     closeDescTag +
     openInfoTag +
-    i +
     closeInfoTag +
+    descTexts[i] +
+    closeDescTag2 +
     openCaptionTag +
     captionTexts[i] +
     closeCaptionTag +
@@ -132,16 +141,23 @@ for (var i = 0; i < numOfPhotos; i++) {
 document.getElementById("album").innerHTML = imageList.join("");
 
 //info box code
-var getDivDesc = document.querySelector(".desc");
-
-//event listener
-getBoxcloseBox.addEventListener("click",closeBox);
+// var getDivDesc = document.getElementById("descBox");
+var getInfoBox = document.querySelector(".infoBox"); 
 
 //display text box
 function displayInfo(i) {
-  getInfoBox.innerHTML = "";
+  getInfoBox.style.visibility = "visible";
+  getInfoBox.innerHTML = 
+  "<div><h1 class='boxHeader'>"+ petHdrs[i]+"</h1>"+
+  "<p class='boxTxt'>"+petTxts[i]+"</p></div>"+
+  "<div class='closeBox'>Close This Box</div>";
+  
 }
 //close text box
+var getCloseBox = document.querySelector(".closeBox");
+// S
+getCloseBox.addEventListener("click",closeBox);
+
 function closeBox() {
   getInfoBox.style.visibility = "hidden";
 }
