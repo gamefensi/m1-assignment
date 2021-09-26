5ddcabb28df3e42dabc6f1f5bdf6d9cf231f200f
gamefensi <38190915+gamefensi@users.noreply.github.com>
2021-09-25 22:41:06 -0700

    final commit

--- js/gallery.js
+++ js/gallery.js
@@ -8,61 +8,61 @@ var closeList = "</li>"; //Declare a variable to contain a close list tag
 var openCaptionTag = "<figcaption>";
 var closeCaptionTag = "</figcaption>";
 var openDescTag = "<div id='descBox' class='desc' onclick='displayInfo(";
-var closeDescTag = ")'>"
-var closeDescTag2 = "</div>"
+var closeDescTag = ")'>";
+var closeDescTag2 = "</div>";
 
 // Pet 1 info
-var petHdr1 = "Stacy"
+var petHdr1 = "Stacy";
 var petCap1 = "Stacy<p>Sex: N <br>Age: 12 Years</p>";
 var petDesc1 = "Stacy is a 12 year old Chocolate White Dachshund Mix.";
 var petInfo1 = "Stacy's info";
 
 // Pet 2 info
-var petHdr2 = "Josh"
+var petHdr2 = "Josh";
 var petCap2 = "Josh <p>Sex: N<br>Age: 6 Years</p>";
 var petDesc2 = "Josh is a 6 year old Fawn White Boxer Mix.";
 var petInfo2 = "Josh's info";
 // Pet 3 info
-var petHdr3 = "Jake"
+var petHdr3 = "Jake";
 var petCap3 = "Jake  <p>Sex: N<br>Age: 11 Years</p>";
 var petDesc3 = "Jake is an 11 year old Black White Siberian Husky";
 var petInfo3 = "Jake's info";
 // Pet 4 info
-var petHdr4 = "Kevin"
+var petHdr4 = "Kevin";
 var petCap4 = "Kevin  <p>Sex: N<br>Age: 6 Years</p>";
 var petDesc4 = "Kevin is a 6 year old White Siberian Husky";
 var petInfo4 = "Kevin's info";
 // Pet 5 info
-var petHdr5 = "Trunks"
+var petHdr5 = "Trunks";
 var petCap5 = "Trunks <p>Sex: M<br>Age: 6 Years</p>";
 var petDesc5 = "Trunks is a 6 year old Black White Poodle Min Havanese";
 var petInfo5 = "Trunks's info";
 // Pet 6 info
-var petHdr6 = "Chris"
+var petHdr6 = "Chris";
 var petCap6 = "Chris <p>Sex: M<br>Age: 8 Years</p>";
 var petDesc6 = "Chris is a 8 year old Fawn Black German Shepherd Mix";
-var petInfo6 = "Chris's info"
+var petInfo6 = "Chris's info";
 // Pet 7 info
-var petHdr7 = "Jesse"
+var petHdr7 = "Jesse";
 var petCap7 = "Jesse  <p>Sex: M<br>Age: 4 Years</p>";
-var petDesc7 ="Jesse is a 4 year old Black Brown Germ Shepherd Siberian Husky";
+var petDesc7 = "Jesse is a 4 year old Black Brown Germ Shepherd Siberian Husky";
 var petInfo7 = "Jesse's info";
 // Pet 8 info
-var petHdr8 = "Sully"
+var petHdr8 = "Sully";
 var petCap8 = "Sully  <p>Sex: N<br>Age: 3 Years</p>";
 var petDesc8 = "Sully is a 3 year old Black White Siberian Husky";
 var petInfo8 = "Sully's info";
 // Pet 9 info
-var petHdr9 = "Jill"
+var petHdr9 = "Jill";
 var petCap9 = "Jill  <p>Sex: F<br>Age: 4 Years</p>";
 var petDesc9 = "Jill is a 4 year old Red White Black Mouth Cur Mix";
 var petInfo9 = "Jill's info";
 // Pet 10 info
-var petHdr10 = "Christina"
+var petHdr10 = "Christina";
 var petCap10 = "Christina<p>Sex: N<br>Age: 7 Years</p>";
 var petDesc10 = "Christina is a 7 year old Black Shetid Sheepdog Mix";
 var petInfo10 = "Christina's info";
-var openInfoTag = "<div class='infoBox' id='infoBox";
+var openInfoTag = "<div style=visibility:hidden; class='infoBox' id='infoBox";
 var closeInfoTag = "</div>";
 
 var captionTexts = [
@@ -90,7 +90,7 @@ var descTexts = [
   petDesc10,
 ];
 
-var petTxts= [
+var petTxts = [
   petInfo1,
   petInfo2,
   petInfo3,
@@ -100,8 +100,8 @@ var petTxts= [
   petInfo7,
   petInfo8,
   petInfo9,
-  petInfo10
-]; 
+  petInfo10,
+];
 
 var petHdrs = [
   petHdr1,
@@ -113,7 +113,7 @@ var petHdrs = [
   petHdr7,
   petHdr8,
   petHdr9,
-  petHdr10
+  petHdr10,
 ];
 
 //Create a loop to create 6 images starting with index of 0
@@ -129,6 +129,15 @@ for (var i = 0; i < numOfPhotos; i++) {
     openInfoTag +
     i +
     "'>" +
+    "<h1 class='boxHeader'>" +
+    petHdrs[i] +
+    "</h1>" +
+    "<p class='boxTxt'>" +
+    petTxts[i] +
+    "</p>" +
+    "<div class='closeBox' onclick='hideBox(" +
+    i +
+    ")'>Close This Box</div>" +
     closeInfoTag +
     descTexts[i] +
     closeDescTag2 +
@@ -143,24 +152,22 @@ for (var i = 0; i < numOfPhotos; i++) {
 document.getElementById("album").innerHTML = imageList.join("");
 
 //info box code
-// var getDivDesc = document.getElementById("descBox");
-// var getInfoBox = document.getElementById("infoBox"); 
 
 //display text box
 
 function displayInfo(i) {
-  document.getElementById("infoBox"+i).innerHTML = 
-  "<h1 class='boxHeader'>"+ petHdrs[i]+"</h1>"+
-  "<p class='boxTxt'>"+petTxts[i]+"</p>"+
-  "<div id='closeBox' onclick='hideBox("+i+")'>Close This Box</div>";
-  document.getElementById("infoBox"+i).style.visibility = "visible";
-  // document.getElementById("infoBox"+i).style.display = "block";
-};
+  var x = document.getElementById("infoBox" + i);
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
 
 //close text box
 // document.getElement("closeBox").onclick = hideBox;
 
 function hideBox(i) {
-  document.getElementById("infoBox"+i).style.visibility = "hidden";
-  document.getElementById("infoBox"+i).style.display = "none";
-};
+  var x = document.getElementById("infoBox" + i);
+  x.style.visibility = "visible";
+}
