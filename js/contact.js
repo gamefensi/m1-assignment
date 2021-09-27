10ed644fafd9d4b70428abf66a4fc4ab26a57e74
gamefensi <38190915+gamefensi@users.noreply.github.com>
2021-09-26 21:07:34 -0700

    finished code to display conditional field for response

--- js/contact.js
+++ js/contact.js
@@ -4,12 +4,12 @@ var dropDownContainer = document.getElementById("chooseContact");
 var phone = document.getElementById("phoneContact");
 var email = document.getElementById("emailContact");
 
-
-dropDown.addEventListener("select", function () {
-  if (document.querySelector("div.chooseContact") === "Phone") {
-    phone.disabled = "false";
-    email.disabled = "true";
-  } else if (which === "Email") {
+dropDown.addEventListener("change", function () {
+  var dropDownChoice = document.getElementById("choices").value;
+  if (dropDownChoice === "3") {
+    phone.style.display = "block";
+    email.style.display = "none";
+  } else if (dropDownChoice === "2") {
     email.style.display = "block";
     phone.style.display = "none";
   } else {
@@ -17,3 +17,23 @@ dropDown.addEventListener("select", function () {
     phone.style.display = "none";
   }
 });
+
+
+
+
+
+//alt to use with onchange
+// onchange="displayField(this)"
+
+// function displayField (x) { 
+//   if (x.value === "3") {
+//     phone.style.display = "block";
+//     email.style.display = "none";
+//   } else if (x.value === "2") {
+//     email.style.display = "block";
+//     phone.style.display = "none";
+//   } else {
+//     email.style.display = "none";
+//     phone.style.display = "none";
+//   }
+// };
